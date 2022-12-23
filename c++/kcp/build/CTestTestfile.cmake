849d505971967043e376a934959cf89f6c8b24c6
lironghui <707568633@qq.com>
2022-12-23 21:44:01 +0800

    tijiao

---
+++ c++/kcp/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/hgfs/ubuntu/vip/20210313-kcp/kcp_learn
+# Build directory: /mnt/hgfs/ubuntu/vip/20210313-kcp/kcp_learn/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
